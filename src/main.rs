24fc2a7db0be2cc3c22f8b45e12ef09f23cb7a5a
Julius <jupster18@gmx.de>
2023-11-02 21:13:33 +0100

    generating

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{fs::File, io::Write};
+
 fn main() {
     println!("Hello, world!");
+    let mut v32: Vec<u32> = vec![];
+    let mut v: Vec<u8> = vec![];
+    for i in 0..100 {
+        v32.push(i*100)
+    }
+    for byte in v32 {
+        v.push(((byte &0xFF)).try_into().unwrap());
+        v.push((((byte >> 8) &0xFF)).try_into().unwrap());
+        v.push((((byte >> 16) &0xFF)).try_into().unwrap());
+        v.push((((byte >> 24) &0xFF)).try_into().unwrap());
+    }
+    let mut file = File::create("generated.ion").unwrap();
+    file.write_all(&v).unwrap();
 }
+
+// Format: Little-Endian 4-Byte chunks representing u32s
